bb679f8ea884dd9e5b63fc7da5e5183834554949
Kyra Palmer <65500788+kyrapalmer@users.noreply.github.com>
2021-06-07 13:50:52 -0500

    Update reviews.controller.js

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -1,21 +1,37 @@
 const service = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-async function list(req, res, next) {
-    res.json({ data: await service.list() });
-}
-
-async function update(req, res, next) {
-    const { reviewId } = req.params;
-    res.json({ data: await service.update(reviewId) });
+async function reviewExists(req, res, next) {
+  const { reviewId } = req.params;
+  const foundReview = await service.read(reviewId);
+  if (foundReview) {
+    res.locals.review = foundReview;
+    return next();
+  }
+  return next({
+    status: 404,
+    message: `Review cannot be found.`,
+  });
 }
 
 async function destroy(req, res, next) {
-    await service.delete(res.locals.review.review_id);
-    res.sendStatus(204);
+  await service.delete(res.locals.review.review_id);
+  res.sendStatus(204).json("?");
+}
+
+async function list(req, res, next) {
+    const { movieId } = req.params;
+  const result = await service.list(movieId);
+  res.json({ result });
+}
+
+async function update(req, res, next) {
+  const { reviewId } = req.params;
+  res.json({ data: await service.update(reviewId) });
 }
 
 module.exports = {
-    update: asyncErrorBoundary(update),
-    delete: destroy,
-}
+  delete: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(destroy)],
+  list: asyncErrorBoundary(list),
+  update: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(update)],
+};
