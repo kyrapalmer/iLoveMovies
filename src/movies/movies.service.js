d0b90972bd68b8410b6a979f9db64f570b782be9
Kyra Palmer <65500788+kyrapalmer@users.noreply.github.com>
2021-06-07 13:50:31 -0500

    Update movies.service.js

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,9 +1,28 @@
 const knex = require("../db/connection");
 
-function list() {
-    return knex("movies").select("*");
+//LIST
+function list(is_showing) {
+  return knex("movies")
+    .select("movies.*")
+    .modify((queryBuilder) => {
+      if (is_showing) {
+        queryBuilder
+          .join(
+            "movies_theaters",
+            "movies.movie_id",
+            "movies_theaters.movie_id"
+          )
+          .where({ "movies_theaters.is_showing": true })
+          .groupBy("movies.movie_id");
+      }
+    });
 }
 
-module.exports = {
-    list,
+//READ
+function read(movieId) {
+  return knex("movies").where("movie_id", movieId);
 }
+module.exports = {
+  list,
+  read,
+};
