3ae995bf2ab8c26ff667132f882fc008206e7c0c
Kyra Palmer <65500788+kyrapalmer@users.noreply.github.com>
2021-06-07 13:51:29 -0500

    Update reviews.service.js

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -1,24 +1,45 @@
 const knex = require("../db/connection");
 
-function list() {
-    return knex("reviews").select("*");
+//DELETE
+async function destroy(reviewId) {
+  return knex("reviews").where({ review_id: reviewId }).del();
 }
 
-function update(reviewId, updatedReview) {
-    return knex("reviews")
-        .select("*")
-        .where({ review_id: reviewId })
-        .update(updatedReview, "*")
+//LIST
+async function list(movie_id) {
+  return knex("reviews")
+    .where({ movie_id })
+    .then((reviews) => Promise.all(reviews.map(setCritic)));
 }
 
-function destroy(reviewId) {
+//READ
+async function read(reviewId) {
+  return knex("reviews").where({ review_id: reviewId }).first();
+}
+
+//UPDATE
+async function update(review) {
     return knex("reviews")
-        .where({ review_id: reviewId })
-        .del();
+      .select("*")
+      .where({ review_id: review.review_id })
+      .update(review, "*")
+      .then(() => read(review.review_id))
+      .then(setCritic);
+  }
+
+//MIDDLEWARE
+async function readCritic(critic_id) {
+  return knex("critics").where({ critic_id }).first();
+}
+
+async function setCritic(review) {
+  review.critic = await readCritic(review.critic_id);
+  return review;
 }
 
 module.exports = {
-    list,
-    update,
-    delete: destroy,
-}
+  delete: destroy,
+  list,
+  read,
+  update,
+};
