db6c5c658747ac1731a21480ed54b2a097e25c40
Kyra Palmer <65500788+kyrapalmer@users.noreply.github.com>
2021-06-14 11:56:43 -0500

    Update theaters.service.js

--- src/theaters/theaters.service.js
+++ src/theaters/theaters.service.js
@@ -8,11 +8,11 @@ function list() {
 
 //GET
 function getMovies(theater) {
-    return knex("theaters as t")
-        .join("movies_theaters as mt", "t.theater_id", "mt.theater_id")
-        .join("movies as m", "m.movie_id", "mt.movie_id")
-        .select("m.*")
-        .where("t.theater_id", "=", theater.theater_id);
+    return knex("theaters")
+        .join("movies_theaters", "theaters.theater_id", "movies_theaters.theater_id")
+        .join("movies", "movies.movie_id", "movies_theaters.movie_id")
+        .select("movies.*")
+        .where("theaters.theater_id", "=", theater.theater_id);
 }
 
 
@@ -28,4 +28,4 @@ module.exports = {
     list,
     listMovieById,
     getMovies
-}
+};
